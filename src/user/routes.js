515d323320623c9f4d63049c623c69cf0ac475cf
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-24 13:33:38 +0100

    enhancement: added to routes and controllers, and  protected some routes

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,14 +1,24 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { hashPass, comparePass } = require("../middleware/");
+const { hashPass, comparePass, verifyToken } = require("../middleware/");
 
-const { findAllUsers, registerNewUser, loginUser } = require("./controllers");
+const {
+  findAllUsers,
+  registerNewUser,
+  loginUser,
+  deleteUser,
+  updateUser,
+} = require("./controllers");
 
 userRouter.get("/", findAllUsers);
 
 userRouter.post("/register", hashPass, registerNewUser);
 
-userRouter.post("/login", comparePass, loginUser);
+userRouter.post("/login", comparePass, verifyToken, loginUser);
+
+userRouter.delete("/", verifyToken, deleteUser);
+
+userRouter.put("/update", verifyToken, updateUser);
 
 module.exports = userRouter;
